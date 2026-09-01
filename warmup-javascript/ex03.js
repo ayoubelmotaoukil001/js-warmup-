5374a0e54f994e697bb14f11bc2d52733db2bc92
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-09-02 00:32:19 +0100

    la troisiem

---
+++ warmup-javascript/ex03.js
@@ -0,0 +1,22 @@
+let count ;
+for(let i  =10 ; i >0 ; i--)
+{
+  count +=  i  + " " ;   
+}
+console.log(`${count}`) ;
+console  .log(`decollage !`)
+let sum  = 0 ;
+for(let i  =0 ; i <= 100 ; i ++)
+{
+    sum += i  ;
+} 
+console.log(`le totale de 1 jusque  100 est ${sum}`) ;
+let res ;
+for ( let  i  =0  ; i <=20 ;  i++)
+{
+    if( i %2 == 0)
+    {
+       res +=  i  + " " ;
+    }
+}
+console.log(`les nbr paires  est : ${res}`) ;
