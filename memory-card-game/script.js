349534e913e28d5a52f4ff8ad0e14b9bda2637ba
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-09-04 14:40:36 +0100

    feat: add card flipping logic and track move count

--- memory-card-game/script.js
+++ memory-card-game/script.js
@@ -7,39 +7,60 @@ const cards = [
     { id: "node", image: "https://placehold.co/150x200/339933/ffffff?text=Node" }
 ];
 
-const cardGrid = document.getElementById("grid") ;
-const restartButoon = document.getElementById("restart") ;
-const  moves =  document.getElementById("moves") ;
-const paires = document.getElementById("pairs") ;
-const winMessage = document.getElementById("win-message") ;
+const cardGrid = document.getElementById("grid");
+const restartButoon = document.getElementById("restart");
+const moves = document.getElementById("moves");
+const paires = document.getElementById("pairs");
+const winMessage = document.getElementById("win-message");
 
-let flippedCards =  [] ;
-let movesCount =0 ;
-let pairesCount = 0 ;
-let boardLokc = false ;
+let flippedCards = [];
+let movesCount = 0;
+let pairesCount = 0;
+let boardLokc = false;
 
-function shuffle (array)
-{
-    for (let  i  = array.length-1 ; i >0 ; i--){
-        const j = Math.floor(Math.random() * (i+1)) ;
-        [array[i] , array[j]] = [array[j] , array[i]] ; 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-    return array ;
+    return array;
 }
-function initGame()
-{
-    const gameCards = shuffle([...cards , ...cards]) ;
+function initGame() {
+    const gameCards = shuffle([...cards, ...cards]);
     gameCards.forEach(card => {
-        const cardElement = document.createElement("div") ;
-        cardElement.classList.add("card") ;
-        cardElement.dataset.id = card.id ;
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.dataset.id = card.id;
         cardElement.innerHTML = `
          <div class="card-front">
          <img src="${card.image}" alt="${card.id}">
         </div>
         <div class="card-back">?</div>  `
-        cardElement.addEventListener("click" , ()=> flippedCard(cardElement)) ;
-        cardGrid.appendChild(cardElement) ;
+        cardElement.addEventListener("click", () => flippedCard(cardElement));
+        cardGrid.appendChild(cardElement);
     });
-    
+}
+function flippedCard(cardElement)
+{
+    if(boardLokc == true)
+        {
+            return ;
+        }
+    if(cardElement.classList.contains("flipped"))
+    {
+        return ;
+    }
+    if(cardElement.classList.contains("matched"))
+    {
+        return ;
+    }
+    cardElement.classList.add("flipped") ;
+    flippedCards.push(cardElement);
+
+    if(flippedCards.length== 2)
+    {
+        movesCount++ ;
+        moves.textContent = movesCount ;
+        checkMatch() ;
+    }
 }
